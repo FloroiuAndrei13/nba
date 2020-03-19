89db77d483129963ac724d1957e15e5b56dbf16f
Floroiu Andrei <floandrei21@gmail.com>
2020-03-19 22:46:48 +0200

    third commit

--- src/loose/oose/fis/Main.java
+++ src/loose/oose/fis/Main.java
@@ -28,7 +28,7 @@ public class Main {
 
         Procesor c1 = new ProcesorCautare("text1");
         Procesor c2 = new ProcesorCautare("text3");
-        Procesor c3 = new ProcesorCautare("text2");
+        //Procesor c3 = new ProcesorCautare("text2"); MODIFICARE
 
         ProcesorCompus pc1 = new ProcesorCompus();
         pc1.adaugaProcesor(c1);
@@ -36,7 +36,7 @@ public class Main {
 
         ProcesorCompus pc2 = new ProcesorCompus();
         pc2.adaugaProcesor(pc1);
-        pc2.adaugaProcesor(c3);
+        //pc2.adaugaProcesor(c3);
 
         System.out.println(pc2.proceseaza(documente));
     }
